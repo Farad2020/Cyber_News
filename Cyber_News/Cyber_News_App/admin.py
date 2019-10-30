9c22ce5f3b86bf2821914344b51b703eb3c95b78
Farad2020 <fnirc@mail.ru>
2019-10-31 02:59:12 +0600

    More models

--- Cyber_News/Cyber_News_App/admin.py
+++ Cyber_News/Cyber_News_App/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Article,Editor
+from .models import Article, Thread, Blog, Comment, Editor, User
 # Register your models here.
 admin.site.register(Article)
+admin.site.register(Thread)
+admin.site.register(Blog)
+admin.site.register(Comment)
 admin.site.register(Editor)
-
+admin.site.register(User)
--- Cyber_News/Cyber_News_App/models.py
+++ Cyber_News/Cyber_News_App/models.py
@@ -9,8 +9,33 @@ class Article(models.Model):
     article_date = models.DateTimeField("date published")
 
 
+class Blog(models.Model):
+    blog_id = models.CharField(max_length=9999)
+    blog_text = models.TextField()
+    blog_date = models.DateTimeField("date published")
+    blog_author = models.CharField(max_length=9999)
+
+
+class Thread(models.Model):
+    thread_id = models.CharField(max_length=9999)
+    thread_text = models.TextField()
+    thread_date = models.DateTimeField("date published")
+    thread_author = models.CharField(max_length=9999)
+
+
+class Comment(models.Model):
+    comment_id = models.CharField(max_length=9999)
+    comment_text = models.TextField()
+    comment_date = models.DateTimeField("date published")
+
+
 class Editor(models.Model):
     editors_name = models.CharField(max_length=100)
     editors_surname = models.CharField(max_length=100)
     editor_id = models.CharField(max_length=1000)
 
+
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_surname = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=1000)
