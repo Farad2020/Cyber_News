e8ff4ce224dc8b0ba9e950b0232c32dec37612c4
niko-alua <alua20006@gmail.com>
2019-10-31 22:47:09 +0300

    improved models

--- Cyber_News/Cyber_News_App/models.py
+++ Cyber_News/Cyber_News_App/models.py
@@ -8,12 +8,18 @@ class Article(models.Model):
     article_text = models.TextField()
     article_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.article_text + self.article_date
+
 
 class Blog(models.Model):
     blog_id = models.CharField(max_length=9999)
     blog_text = models.TextField()
     blog_date = models.DateTimeField("date published")
     blog_author = models.CharField(max_length=9999)
+
+    def __str__(self):
+        return self.blog_author + self.blog_text + self.blog_date
 
 
 class Thread(models.Model):
@@ -22,11 +28,17 @@ class Thread(models.Model):
     thread_date = models.DateTimeField("date published")
     thread_author = models.CharField(max_length=9999)
 
+    def __str__(self):
+        return self.thread_author + self.thread_text + self.thread_date
+
 
 class Comment(models.Model):
     comment_id = models.CharField(max_length=9999)
     comment_text = models.TextField()
     comment_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.comment_text + self.comment_date
 
 
 class Editor(models.Model):
@@ -34,8 +46,14 @@ class Editor(models.Model):
     editors_surname = models.CharField(max_length=100)
     editor_id = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.editors_name + self.editors_surname
+
 
 class User(models.Model):
     user_name = models.CharField(max_length=100)
     user_surname = models.CharField(max_length=100)
     user_id = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.user_name + self.user_surname
--- Cyber_News/Cyber_News_App/views.py
+++ Cyber_News/Cyber_News_App/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
     return HttpResponse(
         "I hope this will work"
+        article = Article.objects
     )
