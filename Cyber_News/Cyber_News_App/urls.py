7b80fab15208c39934b218bd091a16db3df78eb1
niko-alua <alua20006@gmail.com>
2019-11-08 14:01:24 +0300

    logreg

--- Cyber_News/Cyber_News_App/urls.py
+++ Cyber_News/Cyber_News_App/urls.py
@@ -7,15 +7,15 @@ app_name = "cyber_news"
 urlpatterns = [
     # GIANT QUESTION!!! why replacing urls with positions fixed the program?
     path('article/<article_id>/', views.article_details, name='article'),
+    path('signup/', views.sign_up, name='sign_up'),
     path('home/', views.homepage),
-    path('login/', views.login),
+    path('login/', views.login_view, name = 'login'),
+    path('userpage/', views.user_page, name = 'user_page'),
     path('games_page/', views.games_page, name="games"),
     path('games_page/<game_id>/', views.game_details, name='game_det'),
     path('games_page/create_game', views.create_game_page),
-    url('', views.index, name="index"),
-
+    url('', views.index, name='index'),
 ]
-    #path('archive/', views.archive, name = 'archive'),
-    #path('article/<int:article_id>/comment', views.addComment, name='addComment'),
-    #path('contacts/', views.contacts, name="contacts")
-
+# path('archive/', views.archive, name = 'archive'),
+# path('article/<int:article_id>/comment', views.addComment, name='addComment'),
+# path('contacts/', views.contacts, name="contacts")
--- Cyber_News/Cyber_News_App/views.py
+++ Cyber_News/Cyber_News_App/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 from .models import *
 
 
@@ -19,28 +20,49 @@ def article_details(request, article_id):
     article.numberOfClicks += 1
     article.save()
 
-    author = User.objects.get(pk=article.author_id_id)    # why id_id works!&?
+    author = User.objects.get(pk=article.author_id_id)  # why id_id works!&?
     return render(request, 'Cyber_News_App/article_detail.html', {'article': article,
                                                                   'author': author})
 
 
-def login(request):
+def sign_up(request):
     # return HttpResponse("user login page")
-    return render(request, 'Cyber_News_App/logregpage.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            redirect('cyber_news:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'Cyber_News_App/signup_page.html', {'form': form})
 
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)   
+            redirect('cyber_news:index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'Cyber_News_App/login.html', {'form': form})
+
+def user_page(request, user_id):
+    user = get_object_or_404(User, pk = user_id)
+    user.save()
+    return render(request, 'Cyber_News_App/login.html', {'user':user})
 
 def games_page(request):
     games = Game.objects.all().order_by('game_name')
     return render(request, "Cyber_News_App/games_page.html", {'games': games})
 
-def create_game_page(request):
 
+def create_game_page(request):
     return render(request, )
 
 
 def game_details(request, game_id):
     game = get_object_or_404(Game, pk=game_id)
     return render(request, "Cyber_News_App/game_details_page.html", {'game': game})
-
-
-
