3c53da2ecaa650ae04d34c41b3eed77fc2639a45
niko-alua <alua20006@gmail.com>
2019-11-14 22:44:19 +0300

    User Application Updated

--- Cyber_News/Cyber_News/settings.py
+++ Cyber_News/Cyber_News/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = 'User_Account.User'
+AUTH_USER_MODEL = 'User_Account.SimpleUser'
 LOGIN_REDIRECT_URL = "cyber_news:index"
 LOGOUT_REDIRECT_URL = "cyber_news:index"
 
