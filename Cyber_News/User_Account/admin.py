4bd88000655ed17dab725906b780f5594079474a
niko-alua <alua20006@gmail.com>
2019-11-14 22:45:37 +0300

    User Application Updated

--- Cyber_News/User_Account/admin.py
+++ Cyber_News/User_Account/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from Cyber_News.Cyber_News.User_Account.forms import SimpleUserForm, SimpleUserChangeForm
+from .forms import *
 from .models import *
-
 # Register your models here.
 
 class CustomizedAdmin(UserAdmin):
