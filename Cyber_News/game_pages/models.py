58079250bbdd16057d835f2d9189ce9df98599cd
Farad2020 <fnirc@mail.ru>
2019-11-14 23:06:32 +0600

    adding app

--- Cyber_News/game_pages/models.py
+++ Cyber_News/game_pages/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    game_name = models.CharField(max_length=1000)
+    game_developer = models.CharField(max_length=1000)
+    game_text = models.TextField(default="")
+    game_publisher = models.CharField(max_length=1000)
+    game_rd = models.DateField('date published') # released date
+    game_rating = models.FloatField(default=0.0)
+    game_img = models.ImageField(upload_to='photos', verbose_name='My Photo')
+    # link to articles, blogs, threads
+    # image(ImageField), platforms(list of strings)
+
+    def __str__(self):
+        return self.game_name
+
+    def rate_game(self, new_rate):
+        if self.rating == 0.0:
+            self.rating = new_rate
+            return self.rating
+        else:
+            return (self.rating + new_rate) / 2
+
+    @classmethod
+    def create(self, game_name, game_developer, game_text, game_publisher, game_rd, game_rating):
+        game = self(game_name=game_name, game_developer=game_developer, game_text=game_text,
+                    game_publisher=game_publisher, game_rd=game_rd, game_rating=game_rating)
+        return game
+
