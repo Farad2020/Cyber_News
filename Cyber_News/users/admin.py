daf11bc64a1c9e8a4c323298de39425a32606c66
niko-alua <alua20006@gmail.com>
2019-11-15 10:18:59 +0300

    Test

--- Cyber_News/users/admin.py
+++ Cyber_News/users/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 from .forms import *
 from .models import *
 
-
 admin.site.register(User, CustomizedAdmin)
--- Cyber_News/users/urls.py
+++ Cyber_News/users/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path('register/', views.register, name = 'register'),
-    path('profile/', views.profile, name = 'profile'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
