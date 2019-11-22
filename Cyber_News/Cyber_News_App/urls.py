fea96cb29b80284d63b7d696f8b439780c9245c1
Farad2020 <fnirc@mail.ru>
2019-11-22 06:21:28 +0600

    pics

--- Cyber_News/Cyber_News_App/urls.py
+++ Cyber_News/Cyber_News_App/urls.py
@@ -6,19 +6,8 @@ from django.contrib.auth import views as auth_views
 app_name = "cyber_news"
 
 urlpatterns = [
-    # GIANT QUESTION!!! why replacing urls with positions fixed the program?
     path('article/<article_id>/', v1.article_details, name='article'),
-    # path('signup/', views.sign_up, name='sign_up'),
     path('home/', v1.homepage),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url('', v1.index, name="index"),
 ]
-# path('archive/', views.archive, name = 'archive'),
-# path('article/<int:article_id>/comment', views.addComment, name='addComment'),
-# path('contacts/', views.contacts, name="contacts")
-'''
-    path('games_page/', views.games_page, name="games"),
-    path('games_page/<game_id>/', views.game_details, name='game_det'),
-    # some trouble with path games_page/create_game/
-    path('games_page//create_game/', views.create_game_page, name='game_creation'),
-'''
