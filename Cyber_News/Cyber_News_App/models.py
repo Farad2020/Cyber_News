07ac7987148c662c31316289fe33d5321d9464b4
Farad2020 <fnirc@mail.ru>
2019-11-28 21:29:22 +0600

    stuff

--- Cyber_News/Cyber_News_App/models.py
+++ Cyber_News/Cyber_News_App/models.py
@@ -21,15 +21,15 @@ class Editor(models.Model):
 
 
 class Article(models.Model):
-    article_name = models.CharField(max_length=1000)
-    article_text = models.TextField(default="")
-    article_date = models.DateTimeField(auto_now_add=True)  # earlier was written this: 'date published'
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    #game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
-    rating = models.FloatField(default=0.0)
-    numberOfClicks = models.IntegerField(default=0)
-    isBlog = models.BooleanField(True)
-    # game_link
+
+    # game_link    article_name = models.CharField(max_length=1000)
+    #     article_text = models.TextField(default="")
+    #     article_date = models.DateTimeField(auto_now_add=True)  # earlier was written this: 'date published'
+    #     author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    #     #game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    #     rating = models.FloatField(default=0.0)
+    #     numberOfClicks = models.IntegerField(default=0)
+    #     isBlog = models.BooleanField(True)
     # article
     # image optional
 
