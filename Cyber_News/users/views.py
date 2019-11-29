6eaa3a01c53c12a20ffe252dce21ceee78df5979
niko-alua <alua20006@gmail.com>
2019-11-29 10:01:02 +0300

    User

--- Cyber_News/users/views.py
+++ Cyber_News/users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 # Create your views here.
 from .forms import *
 
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
 
@@ -40,3 +41,15 @@ def edit_profile(request):
     else:
         form = SimpleUserChangeForm(instance=request.user)
     return render(request, 'users/edit_profile.html', {'form': form})
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/profile')
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'users/change_password.html', {'form': form})
